c9cd4fcce2bc2478928eb6a302ac6d64916eebec
beatrixyu <beatrixyu@126.com>
2020-07-01 09:07:30 +0200

    Updates

---
+++ precache-manifest.7d0063866f2fe3e1f2ac4ada5cb0d7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2188e73fdc57a708a353347ea5b16a",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "3d33981f9029269f6485",
+    "url": "/react-movie-search/static/css/main.23e31083.chunk.css"
+  },
+  {
+    "revision": "a57710c0b3ba086a6a04",
+    "url": "/react-movie-search/static/js/2.d16a060d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.d16a060d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d33981f9029269f6485",
+    "url": "/react-movie-search/static/js/main.3e81da1b.chunk.js"
+  },
+  {
+    "revision": "a59f08babf504ffdaf64",
+    "url": "/react-movie-search/static/js/runtime-main.f18536cc.js"
+  }
+]);
